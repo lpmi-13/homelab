2a8468a8414b85ebbf33c0e40f44722f4fd1c1f9
Adam Leskis <leskis@gmail.com>
2021-10-13 23:02:58 +0100

    add script for generating ed25519 ssh keys

---
+++ create_keys.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+if [ $# -eq 0 ]; then
+  echo "please pass in a name for the generated keys, for example: \n ./create_keys.sh pihole"
+
+  exit 1
+fi
+
+KEY_NAME=$1
+
+ssh-keygen -t ed25519 -o -a 100 -f ansible/$KEY_NAME -N ""
